69eae634d8906fd0d5ed96375357b81ff494355e
Pragya Chauhan <pchauhan@stevens.edu>
2020-03-28 15:43:45 -0400

    Create BODMAS.cpp

---
+++ BODMAS.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 2 + 3 * 4 << '\n';  //14
+	cout << 2 * 3 / 2 << '\n';  //3
+	cout << 2 / 3 * 3 << '\n';  //0
+	cout << 2.0/ 3 *3 << '\n';//Why????????????
+	cout << 2 / 3 * 2.0 << '\n';//0
+}
